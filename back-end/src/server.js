bddd1d648bf2193e0f75c9fd61c336fb26231fe8
Denis <marksdev@gmail.com>
2022-06-02 23:39:29 -0300

    refactor: client db

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,60 +1,67 @@
 import expresss from "express";
 import bodyParser from "body-parser";
-import ClientDb from "./util/MongoClientDb";
+import { withDB } from "./util/MongoClientDb";
 
 const app = expresss();
 app.use(bodyParser.json());
 
-app.post("/api/articles/:name/upvote", async (req, res) => {
-  const { name: articleName } = req.params;
-  const db = await ClientDb("my-blog");
+app.post(
+  "/api/articles/:name/upvote",
+  async (req, res) => {
+    withDB(async (db) => {
+      const { name: articleName } = req.params;
 
-  const articleInfo = await db
-    .collection("articles")
-    .findOne({ name: articleName });
+      const articleInfo = await db
+        .collection("articles")
+        .findOne({ name: articleName });
 
-  if (articleInfo === null) return res.status(202);
+      if (articleInfo === null) return res.status(202);
 
-  await db.collection("articles").updateOne(
-    { name: articleName },
-    {
-      $set: {
-        upvotes: ++articleInfo.upvotes,
-      },
-    }
-  );
+      await db.collection("articles").updateOne(
+        { name: articleName },
+        {
+          $set: {
+            upvotes: ++articleInfo.upvotes,
+          },
+        }
+      );
 
-  res.status(200).json(articleInfo);
-});
+      res.status(200).json(articleInfo);
+    });
+  },
+  () => res.sendStatus(500)
+);
 
 app.get("/api/articles/:name", async (req, res) => {
-  try {
-    const { name: articleName } = req.params;
+  withDB(
+    async (db) => {
+      const { name: articleName } = req.params;
 
-    const db = await ClientDb("my-blog");
+      const articleInfo = await db
+        .collection("articles")
+        .findOne({ name: articleName });
 
-    const articleInfo = await db
-      .collection("articles")
-      .findOne({ name: articleName });
-
-    if (articleInfo === null) return res.sendStatus(404);
-    res.status(200).json(articleInfo);
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
+      if (articleInfo === null) return res.sendStatus(404);
+      res.status(200).json(articleInfo);
+    },
+    () => res.sendStatus(500)
+  );
 });
 
-app.post("/api/articles/:name/add-comment", (req, res) => {
-  const { username, text } = req.body;
-  const articleName = req.params.name;
+app.post(
+  "/api/articles/:name/add-comment",
+  (req, res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
 
-  const article = articlesInfo[articleName];
+    const article = articlesInfo[articleName];
 
-  article.comments.push({ username, text });
+    article.comments.push({ username, text });
 
-  res.status(200).json(article);
-});
+    res.status(200).json(article);
+  },
+  () => res.sendStatus(500)
+);
 
 app.listen(8000, () => {
   console.log("Listening on port 8000 ");
--- back-end/src/util/MongoClientDb.js
+++ back-end/src/util/MongoClientDb.js
@@ -1,8 +1,21 @@
 import { MongoClient } from "mongodb";
 
-const ClientDb = async (name) => {
+const ClientDb = async () => {
   const client = await MongoClient.connect("mongodb://localhost:27017");
-  return client.db(name);
+  return client;
+};
+
+export const withDB = async (operations, onError = () => {}) => {
+  try {
+    const client = await ClientDb();
+    const db = client.db("my-blog");
+
+    await operations(db);
+    client.close();
+  } catch (error) {
+    console.error(error);
+    onError();
+  }
 };
 
 export default ClientDb;
