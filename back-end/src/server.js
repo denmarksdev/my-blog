dc60fa61bdc05d3f4c40e412fd488e1b6b471cb0
Denis <marksdev@gmail.com>
2022-06-02 22:04:01 -0300

    feat: add comments

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -3,10 +3,36 @@ import bodyParser from "body-parser";
 
 const app = expresss();
 
+const articlesInfo = {
+  "learn-react": {
+    upvotes: 0,
+    comments: [],
+  },
+  "learn-node": {
+    upvotes: 0,
+    comments: [],
+  },
+  "my-thoughts-on-resumes": {
+    upvotes: 0,
+    comments: [],
+  },
+};
+
 app.use(bodyParser.json());
 
-app.get("/hello", (req, res) => {
-  res.send("Hello!");
+app.get("/api/articles/votes", (req, res) => {
+  res.status(200).json(articlesInfo);
+});
+
+app.post("/api/articles/:name/add-comment", (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+
+  const article = articlesInfo[articleName];
+
+  article.comments.push({ username, text });
+
+  res.status(200).json(article);
 });
 
 app.listen(8000, () => {
